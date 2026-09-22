17f447a4cbc4fb898cf2b4448a7152bd7fc260dd
Ali00SDW <sdw98027@gmail.com>
2026-09-22 12:36:24 +0300

    solve 11

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,12 @@
 import "./App.css";
 import Weather from "./Weather";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
 
-const theme = createTheme({
-  components: {
-    MuiTypography: {
-      styleOverrides: {
-        root: {
-          fontFamily: "ui-serif",
-          fontWeight: "bold",
-        },
-      },
-    },
-  },
-});
+
 function App() {
   return (
-    <ThemeProvider theme={theme}>
+
       <Weather/>
-    </ThemeProvider>
+
   );
 }
 
